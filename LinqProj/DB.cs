a1309852f5dc616fc4633e653415b7c30c9b9c44
ksrik <ksrik@MURDOC>
2022-08-02 21:21:54 +0530

    Add project files.

---
+++ LinqProj/DB.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProj
+{
+    public class DB
+    {
+        public int JerseyNum { get; set; }
+        public string Name { get; set; }
+        public bool Home { get; set; }  
+        public List<int> Runs { get; set; }
+
+        public static List<DB> InvokeDB()
+        {
+            DB[] list1 =
+            {new DB
+                {
+                    JerseyNum = 10,
+                    Name = "Sachin",
+                    Home = true,
+                    Runs = new List<int> { 55, 37, 104, 21, 142, 87, 8, 26, 114, 44, 28, 3}
+                },
+                new DB
+                {
+                    JerseyNum = 19,
+                    Name = "Dravid",
+                    Home = true,
+                    Runs = new List<int> { 67, 33, 72, 69, 29, 127, 32, 52, 12, 39, 105, 48 }
+                },
+                new DB
+                {
+                    JerseyNum = 24,
+                    Name = "Ganguly",
+                    Home = true,
+                    Runs = new List<int> { 42, 31, 18, 12, 158, 25, 88, 31, 7, 101, 39, 47 }
+                },
+                new DB
+                {
+                    JerseyNum = 7,
+                    Name = "Dhoni",
+                    Home = true,
+                    Runs = new List<int> { 23, 33, 59, 27, 40, 79, 19, 35, 62, 43, 20, 110 }
+                },
+                new DB
+                {
+                    JerseyNum = 18,
+                    Name = "Kohli",
+                    Home = true,
+                    Runs = new List<int> { 27, 108, 133, 23, 17, 40, 68, 71, 9, 22, 56, 61 }
+                },
+                new DB
+                {
+                    JerseyNum = 45,
+                    Name = "Rohit",
+                    Home = true,
+                    Runs = new List<int> { 12, 12, 75, 176, 5, 42, 52, 36, 103, 21, 23, 64 }
+                },
+                new DB
+                {
+                    JerseyNum = 24,
+                    Name = "Ganguly",
+                    Home = false,
+                    Runs = new List<int> { 28, 11, 22, 9, 96, 66, 111, 6, 15, 68}
+                },
+                new DB
+                {
+                    JerseyNum = 19,
+                    Name = "Dravid",
+                    Home = false,
+                    Runs = new List<int> { 16, 38, 35, 47, 8, 16, 138, 4, 29, 43 }
+                },
+                new DB
+                {
+                    JerseyNum = 10,
+                    Name = "Sachin",
+                    Home = false,
+                    Runs = new List<int> { 54, 32, 87, 10, 7, 128, 109, 6, 14, 29 }
+                },
+                new DB
+                {
+                    JerseyNum = 7,
+                    Name = "Dhoni",
+                    Home = false,
+                    Runs = new List<int> { 7, 31, 9, 69, 32, 37, 77, 25, 24, 49 }
+                },
+                new DB
+                {
+                    JerseyNum = 18,
+                    Name = "Kohli",
+                    Home = false,
+                    Runs = new List<int> { 28, 84, 30, 21, 105, 32, 11, 107, 45, 53 }
+                },
+                new DB
+                {
+                    JerseyNum = 45,
+                    Name = "Rohit",
+                    Home = false,
+                    Runs = new List<int> { 6, 11, 19, 148, 41, 24, 16, 61, 15, 63 }
+                },
+
+            };
+            List<DB>list2 = new List<DB>(list1);
+            return list2;
+        }
+    }
+}
---
+++ LinqProj/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace LinqProj
+{
+    class Program
+    {
+        public static void Main()
+        {
+            IEnumerable<DB> result = DB.InvokeDB().OrderByDescending(s => s.Name).ThenByDescending(s => (s.Runs).Sum());
+            
+            Console.WriteLine("Enter page number");
+            int page = 0;
+
+            if (int.TryParse(Console.ReadLine(), out page))
+            {
+                if (page >= 1 && page <= 6)
+                {
+                        int pageSize = 2;
+                        IEnumerable<DB>paging = result.Skip((page - 1)*pageSize).Take(pageSize).ToList();
+                    Console.WriteLine("-------------------------------------------------BATTING STATS-------------------------------------------------");
+                    Console.WriteLine();
+                        Console.WriteLine("Page " + page);
+                    Console.WriteLine();
+                    Console.WriteLine("\t\t\tName" + "\t\t" + "Innings" + "\t\t" + "Runs" + "\t\t" + "50s/100s" + "\t" + "Avg" + "\t\t" + "HS");
+                    Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+                        foreach (var item in paging)
+                        {
+                        if (item.Home)
+                        {
+                            Console.WriteLine("Home stats");
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Away stats");
+                            Console.WriteLine();
+
+                        }
+                        Console.WriteLine("\t\t\t" + item.Name + "\t\t" + item.Runs.Count() + "\t\t" + item.Runs.Sum()
+                            + "\t\t" + item.Runs.Where(s => s > 50 && s < 100).Count() + "/" + 
+                            item.Runs.Where(s => s > 100).Count() + "\t\t" + Math.Round(item.Runs.Average(),2) + "\t\t" + item.Runs.Max());
+                        Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+                        Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+
+
+                    }
+
+                }
+            }
+            else
+            {
+                    Console.Write("Invalid Page Number");
+            }
+            Console.WriteLine();
+            
+            Console.WriteLine();
+            Console.WriteLine("View Overall Batting Stats of:- ");
+            Console.WriteLine();
+            var batter = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"Overall batting Stats of {batter}: ");
+            Console.WriteLine();
+            var result2 = DB.InvokeDB().Where(s => s.Name == batter).SelectMany(s => s.Runs);
+            Console.WriteLine("\tName" + "\t\t" + "Innings" + "\t\t" + "Runs" + "\t\t" + "50s/100s" + "\t" + "Avg" + "\t\t" + "HS");
+            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+            Console.WriteLine("\t" + batter + "\t\t" + result2.Count() + "\t\t" + result2.Sum()
+                            + "\t\t" + result2.Where(s => s > 50 && s < 100).Count() + "/" + 
+                            result2.Where(s => s > 100).Count() + "\t\t" + Math.Round(result2.Average(),2) + "\t\t" + result2.Max());
+            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+        }
+    }
+}
